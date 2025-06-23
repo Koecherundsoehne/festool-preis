3fcfac08d2aaf511e2066c5a23c9a4c55141fb40
Koecherundsoehne <koecherundsoehne@gmail.com>
2025-06-23 10:51:23 +0200

    update_preis.py

--- update_preis.py
+++ update_preis.py
@@ -2,20 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+# Zielseite aufrufen
 url = "https://wta.hoechsmann.com/de/article/214111/posten_handmaschinen_festool_ofk_500_q__ets_150__bs_75_e"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
+# Preis aus .price-Klasse extrahieren
+preis_element = soup.select_one(".price")
 preis = "nicht gefunden"
+if preis_element:
+    preis = preis_element.text.strip()
 
-# Preis aus dem <title> extrahieren (ersatzweise)
-title = soup.title.string if soup.title else ""
-if "€" in title:
-    teile = title.split("€")
-    euro_zahl = teile[0].split()[-1]
-    preis = euro_zahl + " €"
-
-# Zeitformat
+# Aktuelles Datum und Uhrzeit
 zeit = datetime.now().strftime("%d.%m.%Y %H:%M")
 
 # Datei schreiben
